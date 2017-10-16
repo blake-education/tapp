c4779d9792ab1efffff66ee75caf86adf59944a0
Bharanee Rathna <bharanee@blake.com.au>
2017-10-17 08:29:21 +1100

    only print objects if stdout is a tty.

--- lib/tapp.rb
+++ lib/tapp.rb
@@ -4,30 +4,33 @@ rescue LoadError
   require 'pp'
 end
 
-  
-
 class Object
-
   if defined?(::AwesomePrint)
     def tapp(tag=nil)
-      tap { 
-        print "#{tag}=" if tag
-        ap block_given? ? yield(self) : self 
+      tap {
+        if $stdout.tty?
+          print "#{tag}=" if tag
+          ap block_given? ? yield(self) : self
+        end
       }
     end
   else
     def tapp(tag=nil)
-      tap { 
-        print "#{tag}=" if tag
-        pp block_given? ? yield(self) : self 
+      tap {
+        if $stdout.tty?
+          print "#{tag}=" if tag
+          pp block_given? ? yield(self) : self
+        end
       }
     end
   end
 
   def taputs(tag=nil)
-    tap { 
-      print "#{tag}=" if tag
-      puts block_given? ? yield(self) : self 
+    tap {
+      if $stdout.tty?
+        print "#{tag}=" if tag
+        puts block_given? ? yield(self) : self
+      end
     }
   end
 end
